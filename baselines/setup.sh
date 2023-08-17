13c700800075872989e68edc62a7f79ccac0929f
Mateo Bentura <4504663+mateobentura@users.noreply.github.com>
2023-08-17 10:42:57 -0300

    Branch for isolated SpMM testing

--- baselines/setup.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ./bin
-make sddmm_benchmark
-make spmm_benchmark
--- plot/gen_csv.sh
+++
@@ -1,8 +0,0 @@
-
-python spmm_abl_study.py
-python spmm_pres.py
-python sddmm_abl_study.py
-python spmm_all_matrices.py
-python sddmm_all_matrices.py
-python n2n.py
-
